e523d131edb5b4231fb2b47514c72387c8e367a1
David Bacisin <dtb37@case.edu>
2015-01-28 10:18:43 -0500

    Fixed code errors and rotation direction. Rotation calculations are incorrect still

--- catkin/src/example_robot_commander/src/robot_commander_dtb37.cpp
+++ catkin/src/example_robot_commander/src/robot_commander_dtb37.cpp
@@ -4,11 +4,11 @@
 #include <queue>
 #include <math.h>
 
-#define V_MAX		5.0 // m/sec
+#define V_MAX		0.8 // m/sec
 #define V_MIN		0.1 // m/sec
 #define A_MAX		0.1 // m/sec^2
 #define OMEGA_MAX	1.0 // rad/sec
-#define ALPHA_MAX	0.5 // rad/sec^2
+#define ALPHA_MAX	0.2 // rad/sec^2
 #define CMD_FREQ	100 // Hz
 
 /* PathCoord
@@ -29,47 +29,42 @@ public:
  */
 class RobotCommands {
 private:
-	RobotCommands() {} // cannot create instances
-	
-	static std::queue<PathCoord *> path_queue;
-	static ros::Publisher cmd_publisher;
-	static ros::Rate sleep_timer(CMD_FREQ);
+	// singleton
+	static RobotCommands *instance;
+	std::queue<PathCoord *> path_queue;
+	ros::Publisher cmd_publisher;
+	ros::Rate sleep_timer;
 	// Odometry
-	static ros::Subscriber		odom_subscriber;
-	static ros::Time			odom_lastCallback;
-	static double				odom_deltaCallback;
-	static nav_msgs::Odometry	odom_latest;
-	
+	ros::Subscriber		odom_subscriber;
+	ros::Time			odom_lastCallback;
+	double				odom_deltaCallback;
+	nav_msgs::Odometry	odom_latest;
+
 	static void odomCallback(const nav_msgs::Odometry& odom_rcvd) {
 		// track timing of this callback function
-		odom_deltaCallback = (ros::Time::now() - odom_lastCallback).toSec();
-		odom_lastCallback = ros::Time::now();
+		RobotCommands::instance->odom_deltaCallback = (ros::Time::now() - RobotCommands::instance->odom_lastCallback).toSec();
+		instance->odom_lastCallback = ros::Time::now();
 		// warn about large callback times
-		if (odom_deltaCallback > 0.15)
-			ROS_WARN("Long time since last callback: %lf", odom_deltaCallback);
+		if (instance->odom_deltaCallback > 0.15)
+			ROS_WARN("Long time since last callback: %lf", instance->odom_deltaCallback);
 		
 		// store the newest, desirable odometry data
-		odom_latest.twist.twist.linear.x = odom_rcvd.twist.twist.linear.x;
-		odom_latest.twist.twist.angular.z = odom_rcvd.twist.twist.angular.z;
-		odom_latest.pose.pose.position.x = odom_rcvd.pose.pose.position.x;
-		odom_latest.pose.pose.position.y = odom_rcvd.pose.pose.position.y;
-		odom_latest.pose.pose.orientation.z = odom_rcvd.pose.pose.orientation.z;
-		odom_latest.pose.pose.orientation.w = odom_rcvd.pose.pose.orientation.w;
+		instance->odom_latest = odom_rcvd;
 		
 		// output data for debugging
 		ROS_INFO("Current odometry: x = %f, y = %f, phi = %f, v = %f, omega = %f",
-			odom_latest.pose.pose.position.x,
-			odom_latest.pose.pose.position.y,
-			0.0,
-			odom_latest.twist.twist.linear.x,
-			odom_latest.twist.twist.angular.z);
+			odom_rcvd.pose.pose.position.x,
+			odom_rcvd.pose.pose.position.y,
+			atan2(odom_rcvd.pose.pose.orientation.z, odom_rcvd.pose.pose.orientation.w),
+			odom_rcvd.twist.twist.linear.x,
+			odom_rcvd.twist.twist.angular.z);
 	}
-	
-	static double calculateVelocity(const double distance_goal,
-									const double distance_traveled,
-									const double current_twist,
-									const double vmax,
-									const double amax) {
+		
+	double calculateVelocity(const double distance_goal,
+							const double distance_traveled,
+							const double current_twist,
+							const double vmax,
+							const double amax) {
 		double v,
 			distance_remaining = distance_goal - distance_traveled;
 		// should we stop soon?
@@ -77,40 +72,37 @@ private:
 		// distance to halt = 0.5 * amax * t^2
 		// so distance to halt = 0.5 * v^2 / amax
 		double distance_to_halt = 0.5 * current_twist * current_twist / amax;
-		if (distance_remaining <= 0.0) { // we're here, so stop
-			v = 0.0;
-		}
-		else if (distance_remaining <= distance_to_halt) { // then slow down!
+		if (distance_remaining <= distance_to_halt) { // then slow down!
 			// from equations above,
-			v = sqrt(2.0 * amax * distance_remaining);
+			v = ((distance_goal > distance_traveled)? 1.0: -1.0) * sqrt(2.0 * amax * abs(distance_remaining));
 		}
 		else { // otherwise go as fast as possible
 			v = vmax;
 		}
 		// adjust v based off of current velocity so that amax is not violated
-		v -= current_twist;
-		if (v > amax * CMD_FREQ) {
-			v = amax * CMD_FREQ;
-		}
-		else if (v < 0.0) { // let's not go backwards
-			v = 0.0;
+		if ((v - current_twist) > amax * CMD_FREQ) {
+			v = current_twist + amax * CMD_FREQ;
 		}
 		return v;			
 	}
 	
 public:
-	static void init(ros::NodeHandle nh) {
+
+	RobotCommands(ros::NodeHandle nh):
+		sleep_timer(CMD_FREQ)
+	{
+		instance = this;
 		cmd_publisher = nh.advertise<geometry_msgs::Twist>("/robot0/cmd_vel",1);
 		odom_subscriber = nh.subscribe("/robot0/odom", 1, odomCallback);
 		// set angular velocity to ridiculous number so we can tell when it is overwritten with a realistic number
 		odom_latest.twist.twist.angular.z = 1e10;
-	}
+	} 
 	
-	static void add(PathCoord *coord) {
+	void add(PathCoord *coord) {
 		path_queue.push(coord);
 	}
 	
-	static void run() {
+	void run() {
 		geometry_msgs::Twist cmd_twist; // command to send to robot
 		// Initialize cmd_twist
 		cmd_twist.linear.x = 0.0;
@@ -132,7 +124,7 @@ public:
 		// some variables we'll need in the loop 
 		PathCoord *curr;
 		nav_msgs::Odometry odom_start;
-		double dx, dy,
+		double v, dx, dy,
 			   distance_traveled,
 			   distance_remaining;
 		while (ros::ok() &&
@@ -143,7 +135,7 @@ public:
 			odom_start = odom_latest;
 			// first, rotate to the desired heading
 			distance_traveled = 0.0;
-			distance_remaining = curr->relative_heading;
+			distance_remaining = abs(curr->relative_heading);
 			cmd_twist.linear.x = 0.0; // don't move linearly
 			while (ros::ok() &&
 				   distance_remaining > 0.0) {
@@ -157,17 +149,19 @@ public:
 				// send command
 				cmd_publisher.publish(cmd_twist);
 				// update the distance traveled
-				distance_traveled = 2.0 * (atan2(odom_latest.pose.pose.orientation.z,
+				distance_traveled = (atan2(odom_latest.pose.pose.orientation.z,
 												 odom_latest.pose.pose.orientation.w)
 										 - atan2(odom_start.pose.pose.orientation.z,
 												 odom_start.pose.pose.orientation.w));
-				distance_remaining = curr->relative_heading - distance_traveled;
+				distance_remaining = abs(curr->relative_heading - distance_traveled);
 				sleep_timer.sleep(); // sleep
 			}
+			// stop rotating			
+			cmd_twist.angular.z = 0.0; // don't rotate
+			cmd_publisher.publish(cmd_twist);
 			// now, go the desired distance
 			distance_traveled = 0.0;
 			distance_remaining = curr->linear_distance;
-			cmd_twist.angular.z = 0.0; // don't rotate
 			while (ros::ok() &&
 				   distance_remaining > 0.0) {
 				ros::spinOnce(); // allow callbacks to populate fresh data
@@ -175,7 +169,8 @@ public:
 									  distance_traveled,
 									  odom_latest.twist.twist.linear.x,
 									  V_MAX, A_MAX);
-				ROS_INFO("Cmd velocity: %f", v);
+				if (v < 0.0) v = 0.0; // let's not go backwards
+				ROS_INFO("Cmd velocity: %f; distance remaining: %f", v, distance_remaining);
 				cmd_twist.linear.x = v;
 				// send command
 				cmd_publisher.publish(cmd_twist);
@@ -192,20 +187,22 @@ public:
 	}
 };
 
+RobotCommands *RobotCommands::instance;
+
 int main(int argc, char **argv) {
-	ros::init(argc,argv,"robot0_commander"); // name of this node 
+	ros::init(argc,argv,"robot_commander_dtb37"); // name of this node 
 	ros::NodeHandle nh; // two lines to create a publisher object that can talk to ROS
 	//stdr "robot0" is expecting to receive commands on topic: /robot0/cmd_vel
 
 	PathCoord pc1(0.0, 4.8),
-			  pc2(PI/2.0, 12.2),
-			  pc3(PI/2.0, 0.0);
+			  pc2(-M_PI/2.0, 0.0 /*12.2*/),
+			  pc3(M_PI/2.0, 0.0);
 			  
-	RobotCommands::init(nh);
-	RobotCommands::add(&pc1);
-	// RobotCommands::add(&pc2);
-	// RobotCommands::add(&pc3);
-	RobotCommands::run();
+	RobotCommands rc(nh);
+	//rc.add(&pc1);
+	rc.add(&pc2);
+	rc.add(&pc3);
+	rc.run();
 
 	ROS_INFO("Destination reached!");
 
